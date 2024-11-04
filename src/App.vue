1b9ebe9e56d034d4bd90c79155739bba69e201fe
altinaD <altynberdiyeva99@gmail.com>
2024-11-04 16:33:41 +0500

    first things as experiment

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>hello world</div>
+  <PropPassing
+    :tasks="tasks"
+    :mainTitle="mainTitle"
+    @changeStatus="handleStatus"
+  />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import PropPassing from '@/components/ts-components/PropPassing.vue'
+
+interface Task {
+  id: string
+  title: string
+  completed: boolean
+}
+// const mainTitle: string = ref('Learning TypeScript')
+const tasks = ref<Task[]>([
+  { id: '1', title: 'Learn Vue with TypeScript', completed: false },
+  { id: '2', title: 'Build a Vue project', completed: true },
+  { id: '3', title: 'Deploty the project', completed: true },
+])
+
+function handleStatus(id: string) {
+  const task = tasks.value.find(task => task.id === id)
+  if (task) {
+    task.completed = !task.completed
+  }
+}
+</script>
---
+++ src/components/ts-components/PropPassing.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>{{ mainTitle }}:</h1>
+  <ul>
+    <li @click="changeStatus(task.id)" v-for="task in tasks" :key="task.id">
+      {{ task.title }} <br /><span>{{ task.completed }}</span>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+interface Task {
+  id: string
+  title: string
+  completed: boolean
+}
+const props = withDefaults(
+  defineProps<{
+    tasks: Task[]
+    mainTitle: string
+  }>(),
+  {
+    mainTitle: 'This is the default text',
+  },
+)
+
+const emits = defineEmits<{ (e: 'changeStatus', id: string): void }>()
+
+function changeStatus(id: string) {
+  emits('changeStatus', id)
+}
+</script>
